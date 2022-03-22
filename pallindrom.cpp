045693ca73089d87414a14d65fdc1bf8ed6aa924
PRANAY <pranayharishchandra@gmail.com>
2022-03-22 19:47:35 +0530

    learing functions

---
+++ pallindrom.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int pal(string s){
+    int le = s.length();
+    if (le) == 0){
+        return;
+    }
+
+    if s[0]==s[le-1]{
+        return pal(s);
+    }
+
+    else{
+        return ;
+    }
+    return 0;
+}
+
+int main() {
+    string str ;
+    cin>> str;
+    pal(str)
+
+    return 0;
+}
+
